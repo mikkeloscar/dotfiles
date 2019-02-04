9259bc5272017f632febf230b972df44651cf1a4
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2019-02-04 22:58:26 +0100

    Add gnome-screenshot scrnsht script

---
+++ .config/i3/bin/scrnsht_gnome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+directory="$HOME/scrnsht"
+mkdir -p "$directory"
+
+file="$directory/gnome-$(date "+%Y-%m-%d--%H-%I-%S").png"
+# gnome-screenshot --clipboard --area --remove-border "--file=$file"
+gnome-screenshot --area --remove-border "--file=$file"
+# import -quality 4 "$file"
+xclip -selection clipboard -target image/png -i < "$file"
